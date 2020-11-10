0b2fc02846826a2db2f8aa2e6d37b48c6394bb3b
Gianpaolo Gulletta <gianpaologulletta@gmail.com>
2020-11-10 13:36:10 +0000

    bug fixes

--- src/qcpdocumentobject.cpp
+++ src/qcpdocumentobject.cpp
@@ -23,32 +23,54 @@
 **          Version: 1.3.2                                                **
 ****************************************************************************/
 
-#ifndef QCPDOCUMENTOBJECT_H
-#define QCPDOCUMENTOBJECT_H
+#include "../include/qcpdocumentobject.h"
 
-#include <QObject>
-#include <QTextObjectInterface>
-#include <QPicture>
-#include <QVariant>
-#include <QPainter>
-#include <qcustomplot.h>
-
-class QCPDocumentObject : public QObject, public QTextObjectInterface
+QCPDocumentObject::QCPDocumentObject(QObject *parent) :
+  QObject(parent)
 {
-  Q_OBJECT
-  Q_INTERFACES(QTextObjectInterface)
+}
+
+QSizeF QCPDocumentObject::intrinsicSize(QTextDocument *doc, int posInDocument, const QTextFormat &format)
+{
+  Q_UNUSED(doc)
+  Q_UNUSED(posInDocument)
+  QPicture pic = qvariant_cast<QPicture>(format.property(PicturePropertyId));
+  if (pic.isNull())
+  {
+    qDebug() << Q_FUNC_INFO << "Plot object is empty";
+    return QSizeF(10, 10);
+  } else
+    return QSizeF(pic.boundingRect().size());
+}
+
+void QCPDocumentObject::drawObject(QPainter *painter, const QRectF &rect, QTextDocument *doc, int posInDocument, const QTextFormat &format)
+{
+  Q_UNUSED(doc)
+  Q_UNUSED(posInDocument)
+  QPicture pic = qvariant_cast<QPicture>(format.property(PicturePropertyId));
+  if (pic.isNull())
+    return;
   
-public:
-  enum { PlotTextFormat = QTextFormat::UserObject + 3902 }; // if your application already uses the id (QTextFormat::UserObject + 3902), just change the id here
-  enum { PicturePropertyId = 1 };
+  QSize finalSize = pic.boundingRect().size();
+  finalSize.scale(rect.size().toSize(), Qt::KeepAspectRatio);
+  double scaleFactor = finalSize.width()/(double)pic.boundingRect().size().width();
+  painter->save();
+  painter->scale(scaleFactor, scaleFactor);
+  painter->setClipRect(rect);
+  painter->drawPicture(rect.topLeft(), pic);
+  painter->restore();
+}
+
+QTextCharFormat QCPDocumentObject::generatePlotFormat(QCustomPlot *plot, int width, int height)
+{
+  QPicture picture;
+  QCPPainter qcpPainter;
+  qcpPainter.begin(&picture);
+  plot->toPainter(&qcpPainter, width, height);
+  qcpPainter.end();
   
-  explicit QCPDocumentObject(QObject *parent = 0);
-  QSizeF intrinsicSize(QTextDocument *doc, int posInDocument, const QTextFormat &format);
-  void drawObject(QPainter *painter, const QRectF &rect, QTextDocument *doc, int posInDocument, const QTextFormat &format);
-
-  static QTextCharFormat generatePlotFormat(QCustomPlot *plot, int width=0, int height=0);
-
-};
-Q_DECLARE_METATYPE(QPicture)
-
-#endif // QCPDOCUMENTOBJECT_H
+  QTextCharFormat result;
+  result.setObjectType(QCPDocumentObject::PlotTextFormat);
+  result.setProperty(QCPDocumentObject::PicturePropertyId, QVariant::fromValue(picture));
+  return result;
+}
